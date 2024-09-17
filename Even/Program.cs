c8a6991ee5d7c3f3d4dd064512f64f069b2e8caa
asrajafari <168227642+asrajafari@users.noreply.github.com>
2024-09-17 16:37:07 +0330

    commit all

---
+++ Even/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Even
+{
+    static void Main()
+
+    {
+        Console.WriteLine("Enter your choise");
+
+        int choise = int.Parse(Console.ReadLine());
+
+        for (int i = 0 ; i <= 100 ; i++)
+
+            if (i % 2 == 0 )
+            {
+                Console.WriteLine(i+ "Even number");
+            }
+            else
+            {
+                Console.WriteLine(i+ "Odd number");
+                Console.WriteLine("try again");
+                Console.ReadLine();
+            }
+        {
+            
+        }
+    }
+    
+}
+
